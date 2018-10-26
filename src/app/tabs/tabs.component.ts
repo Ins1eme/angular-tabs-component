ea145190a3fd521f649411a49e0981733e85f819
Bogdan Kozitskiy <insieme1337@gmail.com>
2018-10-27 00:09:31 +0300

    Add files via upload

---
+++ src/app/tabs/tabs.component.ts
@@ -0,0 +1,19 @@
+import { Component, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.css']
+})
+export class TabsComponent {
+
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent>
+
+  constructor() { }
+
+  selectTab(tab: TabComponent) {
+    this.tabs.toArray().map((tab) => tab.active = false)
+    tab.active = true
+  }
+}
